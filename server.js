dca30a3a730b5b09abaf65bd390223f65ed30db9
Aman Bhardwaj <ryuzaki@Amans-MacBook-Air.local>
2020-10-06 19:15:48 +0530

    done

---
+++ server.js
@@ -0,0 +1,31 @@
+const webpush = require('web-push');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+require('colors');
+
+const app = express();
+dotenv.config({ path: 'config.env' });
+
+webpush.setVapidDetails(
+  'mailto:test@test.com',
+  process.env.WEB_PUSH_PUBLIC_KEY,
+  process.env.WEB_PUSH_PRIVATE_KEY
+);
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/api', (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({ success: true, msg: 'subscription received' });
+  const payload = JSON.stringify({ title: 'Push Test' });
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.error(err));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`.green.bold);
+});
